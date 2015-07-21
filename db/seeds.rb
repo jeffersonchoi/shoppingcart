1c0ab19ad2d881e377e1346695319dcc3466abc6
Jefferson Choi <jeffersonchoi@gmail.com>
2015-07-21 03:47:43 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create([
+  {first_name: "User 1"},
+  {first_name: "User 2"},
+  {first_name: "User 3"}
+  ])
+Item.create([
+  {name: "Item 1"},
+  {name: "Item 2"},
+  {name: "Item 3"}
+  ])
+User.all.each do |user|
+
+  Order.create(user_id: user.id)
+
+end
+
+Order.all.each do |order|
+  Orderstatus.create(order_id: order.id)
+    Item.all.each do |item|
+      Orderitem.create(order_id: order.id, item_id: item.id)
+    end
+end
